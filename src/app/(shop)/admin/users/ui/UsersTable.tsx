5975d3f1c2a499faec8030d6cdc184d1599d2e3d
hugobepa <hugo_bcn@hotmail.com>
2025-12-05 13:35:23 +0100

    section_24_A_MantAdm_SA_CambiarRolUsuario

--- src/app/(shop)/admin/users/ui/UsersTable.tsx
+++ src/app/(shop)/admin/users/ui/UsersTable.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { changeUserRole } from "@/actions"
 import { User } from "@/interfaces"
 import Link from "next/link"
 import { IoCardOutline } from "react-icons/io5"
@@ -43,7 +44,7 @@ export const UsersTable = ({ users }: Props) => {
                             <td className="flex items-center text-sm  text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                 <select
                                     value={user.role}
-                                    onChange={e =>console.log(e.target.value)} //{changeUserRole(user.id, e.target.value)}
+                                    onChange={e=>changeUserRole(user.id, e.target.value)}
                                     className="text-sm w-full p-2 text-gray-900">
                                     <option value="admin">Admin</option>
                                     <option value="user">User</option>
