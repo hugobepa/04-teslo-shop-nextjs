171f8b62d01c5098bbd8fc13ec961d41e5bd6127
hugobepa <hugo_bcn@hotmail.com>
2025-12-05 17:40:55 +0100

    section_24_A_MantAdm_PantallaProducto

---
+++ src/app/(shop)/admin/product/[slug]/page.tsx
@@ -0,0 +1,32 @@
+import { getProductBySlug } from "@/actions";
+import { Title } from "@/components";
+import { redirect } from "next/navigation";
+import { ProductForm } from './ProductForm';
+
+interface Props{
+    params: Promise<{ slug: string }>
+}
+
+
+export default async function  ProductPage({params}:Props) {
+
+    const {slug} = await params;
+
+    const product = await getProductBySlug(slug);
+
+    //todo: new
+    if(!product) {
+        redirect('/admin/products')
+    }
+
+    const title = (slug === 'new') ? 'Nuevo producto' : 'Editar producto'
+
+  return (
+   <>
+        <Title title={product?.title ?? ''} />
+
+        <ProductForm product={product}/>
+
+    </>
+  );
+}
