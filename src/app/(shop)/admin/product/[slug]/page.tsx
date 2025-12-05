9ec8c7fbd7e8a2f2b330c6d4e1bce098660f41bb
hugobepa <hugo_bcn@hotmail.com>
2025-12-05 19:00:10 +0100

    section_24_A_MantAdm_CargarCategorias

--- src/app/(shop)/admin/product/[slug]/page.tsx
+++ src/app/(shop)/admin/product/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { getProductBySlug } from "@/actions";
+import { getCategories, getProductBySlug } from "@/actions";
 import { Title } from "@/components";
 import { redirect } from "next/navigation";
 import { ProductForm } from './ProductForm';
@@ -12,7 +12,14 @@ export default async function  ProductPage({params}:Props) {
 
     const {slug} = await params;
 
-    const product = await getProductBySlug(slug);
+    const [product,categories] =await Promise.all([
+        getProductBySlug(slug),
+        getCategories(),
+    ])
+
+    
+
+    //get-categories.ts  getCategories
 
     //todo: new
     if(!product) {
@@ -25,7 +32,7 @@ export default async function  ProductPage({params}:Props) {
    <>
         <Title title={product?.title ?? ''} />
 
-        <ProductForm product={product}/>
+        <ProductForm product={product}  categories={categories}/>
 
     </>
   );
