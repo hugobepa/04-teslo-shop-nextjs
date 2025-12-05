76bcb9763f00057a416ab9e98be8116db920b3e2
hugobepa <hugo_bcn@hotmail.com>
2025-12-05 19:35:31 +0100

    section_24_A_MantAdm_FormularioProductos

--- src/app/(shop)/admin/product/[slug]/ProductForm.tsx
+++ src/app/(shop)/admin/product/[slug]/ProductForm.tsx
@@ -2,115 +2,149 @@
 
 import { getCategories } from "@/actions";
 import { Category, Product } from "@/interfaces";
+import { useForm } from "react-hook-form";
 
 interface Props {
-  product: Product;
-  categories: Category[];
- 
+    product: Product;
+    categories: Category[];
+
 }
+
+ interface FormInputs {
+        description: string;
+        inStock: number;
+        price: number;
+        sizes: string[];
+        slug: string;
+        tags: string;
+        title: string;
+        gender: 'men' | 'women' | 'kid' |'unisex';
+        categoryId: string;
+
+        //todo: Imagenes
+    }
+
+
 
 const sizes = ["XS", "S", "M", "L", "XL", "XXL"];
 
-export const ProductForm = ({ product,categories }: Props) => {
+export const ProductForm = ({ product, categories }: Props) => {
 
+    const {
+        handleSubmit,
+        register,
+        formState:{isValid}
+    } = useForm<FormInputs>({
+        defaultValues:{
+            //title: 'este es mi titulo'
+            ...product,
+            tags: product.tags.join(','),
+            sizes: product.sizes ?? [],
 
- 
+            //TODO: images
+        }
+    });
 
-  return (
-    <form className="grid px-5 mb-16 grid-cols-1 sm:px-0 sm:grid-cols-2 gap-3">
-      {/* Textos */}
-      <div className="w-full">
-        <div className="flex flex-col mb-2">
-          <span>Título</span>
-          <input type="text" className="p-2 border rounded-md bg-gray-200" />
-        </div>
+   const onSubmit = async(data: FormInputs)=>{
+    console.log({data})
+   }
 
-        <div className="flex flex-col mb-2">
-          <span>Slug</span>
-          <input type="text" className="p-2 border rounded-md bg-gray-200" />
-        </div>
-
-        <div className="flex flex-col mb-2">
-          <span>Descripción</span>
-          <textarea
-            rows={5}
-            className="p-2 border rounded-md bg-gray-200"
-          ></textarea>
-        </div>
-
-        <div className="flex flex-col mb-2">
-          <span>Price</span>
-          <input type="number" className="p-2 border rounded-md bg-gray-200" />
-        </div>
-
-        <div className="flex flex-col mb-2">
-          <span>Tags</span>
-          <input type="text" className="p-2 border rounded-md bg-gray-200" />
-        </div>
-
-        <div className="flex flex-col mb-2">
-          <span>Gender</span>
-          <select className="p-2 border rounded-md bg-gray-200">
-            <option value="">[Seleccione]</option>
-            <option value="men">Men</option>
-            <option value="women">Women</option>
-            <option value="kid">Kid</option>
-            <option value="unisex">Unisex</option>
-          </select>
-        </div>
-
-        <div className="flex flex-col mb-2">
-          <span>Categoria</span>
-          <select className="p-2 border rounded-md bg-gray-200">
-                <option value="">[seleccione]</option>
-            {
-                categories.map(category=>(
-                    <option key={category.id} value={category.id}>{category.name}</option>
-                ))
-            }
-           
-          </select>
-        </div>
-
-        <button className="btn-primary w-full">
-          Guardar
-        </button>
-      </div>
-
-      {/* Selector de tallas y fotos */}
-      <div className="w-full">
-        {/* As checkboxes */}
-        <div className="flex flex-col">
-
-          <span>Tallas</span>
-          <div className="flex flex-wrap">
-            
-            {
-              sizes.map( size => (
-                // bg-blue-500 text-white <--- si está seleccionado
-                <div key={ size } className="flex  items-center justify-center w-10 h-10 mr-2 border rounded-md">
-                  <span>{ size }</span>
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} className="grid px-5 mb-16 grid-cols-1 sm:px-0 sm:grid-cols-2 gap-3">
+            {/* Textos */}
+            <div className="w-full">
+                <div className="flex flex-col mb-2">
+                    <span>Título</span>
+                    <input type="text" className="p-2 border rounded-md bg-gray-200" {...register('title',{required: true})}/>
                 </div>
-              ))
-            }
 
-          </div>
+                <div className="flex flex-col mb-2">
+                    <span>Slug</span>
+                    <input type="text" className="p-2 border rounded-md bg-gray-200" {...register('slug',{required: true})} />
+                </div>
+
+                <div className="flex flex-col mb-2">
+                    <span>Descripción</span>
+                    <textarea
+                        rows={5}
+                        className="p-2 border rounded-md bg-gray-200"
+                        {...register('description',{required: true})}
+                    ></textarea>
+                </div>
+
+                <div className="flex flex-col mb-2">
+                    <span>Price</span>
+                    <input type="number" className="p-2 border rounded-md bg-gray-200" {...register('price',{required: true, min:0})}/>
+                </div>
+
+                <div className="flex flex-col mb-2">
+                    <span>Tags</span>
+                    <input type="text" className="p-2 border rounded-md bg-gray-200" {...register('tags',{required: true})}/>
+                </div>
+
+                <div className="flex flex-col mb-2">
+                    <span>Gender</span>
+                    <select className="p-2 border rounded-md bg-gray-200" {...register('gender',{required: true})}>
+                        <option value="">[Seleccione]</option>
+                        <option value="men">Men</option>
+                        <option value="women">Women</option>
+                        <option value="kid">Kid</option>
+                        <option value="unisex">Unisex</option>
+                    </select>
+                </div>
+
+                <div className="flex flex-col mb-2">
+                    <span>Categoria</span>
+                    <select className="p-2 border rounded-md bg-gray-200" {...register('categoryId',{required: true})}>
+                        <option value="">[seleccione]</option>
+                        {
+                            categories.map(category => (
+                                <option key={category.id} value={category.id}>{category.name}</option>
+                            ))
+                        }
+
+                    </select>
+                </div>
+
+                <button className="btn-primary w-full">
+                    Guardar
+                </button>
+            </div>
+
+            {/* Selector de tallas y fotos */}
+            <div className="w-full">
+                {/* As checkboxes */}
+                <div className="flex flex-col">
+
+                    <span>Tallas</span>
+                    <div className="flex flex-wrap">
+
+                        {
+                            sizes.map(size => (
+                                // bg-blue-500 text-white <--- si está seleccionado
+                                <div key={size} className="flex  items-center justify-center w-10 h-10 mr-2 border rounded-md">
+                                    <span>{size}</span>
+                                </div>
+                            ))
+                        }
+
+                    </div>
 
 
-          <div className="flex flex-col mb-2">
+                    <div className="flex flex-col mb-2">
 
-            <span>Fotos</span>
-            <input 
-              type="file"
-              multiple 
-              className="p-2 border rounded-md bg-gray-200" 
-              accept="image/png, image/jpeg"
-            />
+                        <span>Fotos</span>
+                        <input
+                            type="file"
+                            multiple
+                            className="p-2 border rounded-md bg-gray-200"
+                            accept="image/png, image/jpeg"
+                        />
 
-          </div>
+                    </div>
 
-        </div>
-      </div>
-    </form>
-  );
+                </div>
+            </div>
+        </form>
+    );
 };
