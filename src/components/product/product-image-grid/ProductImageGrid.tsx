f57376cac37af634e23681f978e9ed2be8295dc5
hugobepa <hugo_bcn@hotmail.com>
2025-12-07 21:41:12 +0100

    section_25_despTienda_mejorarArchivo

--- src/components/product/product-image-grid/ProductImageGrid.tsx
+++ src/components/product/product-image-grid/ProductImageGrid.tsx
@@ -5,42 +5,25 @@ import React, { useState } from 'react'
 import Image from 'next/image';
 
 
-// Import Swiper styles
-
-
-
-
 import { ProductImage } from '../product-image/ProductImage';
 
 interface Props {
     image: string;
     title: string;
-    //className?: string;
     width: number
     height:number
     className?: React.StyleHTMLAttributes<HTMLImageElement>['className'] 
    
 }
 
-// onMouseEnter={()=>setDisplayImage(product.images[1])}
-//         onMouseLeave={()=>setDisplayImage(product.images[0])}
 //onMouseLeave?: MouseEventHandler<HTMLImageElement> | undefined
 export const ProductImageGrid = ({ image, title,className,width,height }: Props) => {
 
-    
-
-    
-
+ 
     return (
 
         <div>
-
-            {/**imagenes grandes */}
-         
-       
-        
-            
-                   <ProductImage
+            <ProductImage
                    src={image} 
                    alt={title} 
                    height={height}
@@ -48,14 +31,6 @@ export const ProductImageGrid = ({ image, title,className,width,height }: Props)
                   className={className}
                 
                    />        
-                
-        
-       
-       
-      
-
-      
-
 
         </div>
 
--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -17,12 +17,12 @@ export const ProductGridItem = ({product}:Props) => {
 
     const [displayImage, setDisplayImage] = useState(product.images[0])
     
-//ProductSlideshow
+
   return (
     <div className="rounded-md overflow-hidden fade-in">
         <Link href={`/product/${product.slug}`}>
 
-
+                {/* //imagen se mueve sola por autoplay */}
              {/* <ProductImageArrayGrid    
                       images={product.images} 
                       title={product.title}
